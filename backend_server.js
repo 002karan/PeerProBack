d35e158891e2f4c0fead7027fea6df072b2728a4
“karan” <kshrivastava002@gmail.com>
2025-04-09 14:10:43 +0530

    rebuild-Backend

--- backend_server.js
+++ backend_server.js
@@ -31,7 +31,7 @@ const server = http.createServer(app);
 // Initialize Socket.IO before using it
 const io = new Server(server, {
     cors: {
-        origin: "*",
+        origin: ["http://localhost:5173", "https://peer-pro-fro.vercel.app"],
         methods: ["GET", "POST"],
         credentials: true
     },
